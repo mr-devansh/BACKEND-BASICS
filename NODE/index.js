353e384ff9700aa10e38b5173f19e57a4c4ea78f
devansh singh <66402296+mr-devansh@users.noreply.github.com>
2022-06-27 22:25:14 +0530

    Add files via upload

---
+++ NODE/index.js
@@ -0,0 +1,8 @@
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var mysuperhero = superheroes.random();
+var mysupervilain = supervillains.random();
+
+console.log(mysuperhero);
+console.log(mysupervilain);
